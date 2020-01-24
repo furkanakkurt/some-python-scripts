a35bae74ad72de82a25995a123c5c5a7ce20a32a
Furkan Akkurt <furkanakkurt1905@gmail.com>
2020-01-24 21:10:04 +0300

    comment

--- lab2/q1.py
+++ lab2/q1.py
@@ -1,3 +1,7 @@
+"""
+Creates and displays a string made of the first 2 and the last 2 chars of a given a string. 
+If the string length is less than 2, the extra spaces are the ! character
+"""
 str = ""
 
 inStr = input("Enter a string ")
